c91a7e7ca67e8cabf7ce8cac876bfb27341855e0
Leo <leo-van@hotmail.com>
2018-04-24 10:23:43 +0800

    调整书籍和影视页面生成方式，实时获取豆瓣和 IMDB 评分等

--- scripts/cn/utils.R
+++ scripts/cn/utils.R
@@ -1,12 +1,14 @@
-library(RCurl)
-library(rjson)
+library(lubridate)
+library(glue)
+library(httr)
+library(jsonlite)
 
 #' 年份 转 年份可读文本
 #' 
 #' @param year 年份数值
 #' @return 年份可读文本
 which_year_ <- function(year) {
-    ifelse(is.na(year), '忘了是哪年', paste0(year, ' 年'))
+    ifelse(is.na(year), '忘了是哪年', glue('{year} 年'))
 }
 
 #' 日期 格式化为 月-日
@@ -14,7 +16,7 @@ which_year_ <- function(year) {
 #' @param date 日期
 #' @return 格式化后的 月-日
 date_month_day_format_ <- function(date) {
-    ifelse(is.na(date), '', paste(month(date), day(date), sep = '-'))
+    ifelse(is.na(date), '', glue('{month}-{day}', month = month(date), day = day(date)))
 }
 
 #' 评分 转 星星
@@ -22,7 +24,8 @@ date_month_day_format_ <- function(date) {
 #' @param 评分 (十分制)
 #' @return 星星 (五星制)
 stars_ <- function(rating) {
-    ifelse(is.na(rating), '', paste0('<div class="star-rating" data-rating="', rating / 2, '"></div>'))
+    ifelse(is.na(rating), '', glue('<span class="star-rating" data-rating="{rating}"></span>',
+                                   rating = rating / 2))
 }
 
 #' 根据 IMDB ID 获取 IMDB 评分
@@ -30,10 +33,12 @@ stars_ <- function(rating) {
 #' @param imdb_id IMDB ID
 #' @return IMDB 评分
 imdb_rating_ <- function(imdb_id) {
-    url <- paste0('http://www.theimdbapi.org/api/movie?movie_id=', imdb_id)
-    res <- getURL(url)
-    res_json <- fromJSON(res)
-    res_json$rating
+    url <- glue('http://www.omdbapi.com/?apikey=31ee42cc&i={imdb_id}')
+    res <- GET(url)
+    res_json <- fromJSON(content(res, as = 'text'))
+    rating <- res_json$imdbRating
+    
+    list(rating = rating, html = glue('<span class="imdb-rating"></span>'))
 }
 
 #' 根据豆瓣 ID 获取豆瓣评分
@@ -41,8 +46,35 @@ imdb_rating_ <- function(imdb_id) {
 #' @param douban_id 豆瓣 ID
 #' @return 豆瓣评分
 douban_rating_ <- function(douban_id) {
-    url <- paste0('https://api.douban.com/v2/movie/subject/', douban_id)
-    res <- getURL(url)
-    res_json <- fromJSON(res)
-    res_json$rating$average
+    url <- glue('https://api.douban.com/v2/movie/subject/{douban_id}')
+    res <- GET(url)
+    res_json <- fromJSON(content(res, as = 'text'))
+    rating <- res_json$rating$average
+    
+    list(rating = rating, html = glue('<span class="douban-rating"></span>'))
+}
+
+#' 获取 IMDB ID 的 HTML 代码
+#' 
+#' @param imdb_id IMDB ID
+#' @return IMDB ID HTML 代码
+imdb_id_html_ <- function(imdb_id) {
+    glue('<span class="imdb-id">{imdb_id}</span>')
+}
+
+#' 获取豆瓣 ID 的 HTML 代码
+#' 
+#' @param douban_id 豆瓣 ID
+#' @return 豆瓣 ID HTML 代码
+douban_id_html_ <- function(douban_id) {
+    glue('<span class="douban-id">{douban_id}</span>')
+}
+
+#' 获取带超链接的文本
+#' 
+#' @param text 文本
+#' @param link 超链接
+#' @return 带超链接的文本
+linked_text_ <- function(text, link) {
+    ifelse(is.na(link), text, text_spec(text, link = link))
 }
