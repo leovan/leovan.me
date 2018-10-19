2ddb6b44fe1cf2862e0b606f59b09cebaf9d7ea7
Leo <leo-van@hotmail.com>
2018-10-19 18:26:15 +0800

    update fonts

--- static/js/load-typekit.js
+++ static/js/load-typekit.js
@@ -11,6 +11,6 @@
     // no need to load TypeKit if Source Hans Serif has been installed
     if (context.measureText(text).width != size) return;
   }
-  var config = {kitId: 'kwz5xar', scriptTimeout: 3000, async: true},
+  var config = {kitId: 'lhj7bub', scriptTimeout: 3000, async: true},
   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
