5d23cafa6890bce57ce9dca254c444cdde6803dc
Leo <leo-van@hotmail.com>
2020-06-30 21:05:55 +0800

    update code copyright

--- scripts/cn/2020-06-13-planning-by-dynamic-programming/car_rental.py
+++ scripts/cn/2020-06-13-planning-by-dynamic-programming/car_rental.py
@@ -3,10 +3,10 @@
 
 #######################################################################
 # Copyright (C)                                                       #
+# 2020 Leo Van (leo-van@hotmail.com)                                  #
 # 2016 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
 # 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
 # 2017 Aja Rangaswamy (aja004@gmail.com)                              #
-# 2020 Leo Van (leo-van@hotmail.com)                                  #
 # Permission given to modify the code as long as you keep this        #
 # declaration at the top                                              #
 #######################################################################
