43afba353af4a5b8ae85ff462bf8ebd80bdc9684
Leo <leo-van@hotmail.com>
2023-01-20 15:41:44 +0800

    optimize link

--- static/js/toggle-theme.js
+++ static/js/toggle-theme.js
@@ -14,13 +14,13 @@ function setTheme(theme) {
   Cookies.set("theme", theme, {path: "/"});
 
   if (theme == "dark") {
-    document.documentElement.className = 'dark';
-    document.getElementById("theme-toggle-icon").classList.remove("mdi-weather-night");
-    document.getElementById("theme-toggle-icon").classList.add("mdi-white-balance-sunny");
+    document.documentElement.className = "dark";
+    document.getElementById("theme-toggle").classList.remove("mdi-weather-night");
+    document.getElementById("theme-toggle").classList.add("mdi-white-balance-sunny");
   } else {
-    document.documentElement.className = '';
-    document.getElementById("theme-toggle-icon").classList.remove("mdi-white-balance-sunny");
-    document.getElementById("theme-toggle-icon").classList.add("mdi-weather-night");
+    document.documentElement.className = "";
+    document.getElementById("theme-toggle").classList.remove("mdi-white-balance-sunny");
+    document.getElementById("theme-toggle").classList.add("mdi-weather-night");
   }
 }
 
