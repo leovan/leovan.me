0a9465bdc011d6e8be6d0b82925f860dc5ff2da4
Leo <leo-van@hotmail.com>
2026-02-01 19:12:26 +0800

    优化主题

--- assets/js/js-after.js
+++ assets/js/js-after.js
@@ -1,1 +1,11 @@
-(() => {})();
+function handleGiscusMessage(event) {
+  if (event.origin !== 'https://giscus.app') return;
+  if (!(typeof event.data === 'object' && event.data.giscus)) return;
+
+  document.querySelectorAll(".giscus-component").forEach((el) => {
+    el.classList.remove("hidden");
+  });
+  window.removeEventListener("message", handleGiscusMessage);
+}
+
+window.addEventListener("message", handleGiscusMessage);
